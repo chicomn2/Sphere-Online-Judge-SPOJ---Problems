dc2080dcf0cc1df0602dbea6c513a92950effb3b
Francisco Nascimento <99285136+chicomn2@users.noreply.github.com>
2022-11-28 20:45:36 -0300

    Update Test1-Life, the Universe, and Everytrhing.cs

--- Test1-Life, the Universe, and Everytrhing.cs
+++ Test1-Life, the Universe, and Everytrhing.cs
@@ -1,4 +1,7 @@
-/*Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, 
+/*
+INTRODUCTION
+
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, 
 and Everything. More precisely... rewrite small numbers from input to output. 
 Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
 
@@ -14,9 +17,12 @@ Output:
 1
 2
 88
+
+SOLUTION
+The Objective here is simply create a code where the user can type any number UNTIL type 42, at this point the program stop
+Code in c#
 */
 
-//Code in c#
 main();
 int main(){
     int input;
